3e52815234cceccac41ac4460fe77e0b427ef6f4
D <1250933584@qq.com>
2020-03-31 19:16:42 +0800

    确认订单页面和添加收货地址页面

--- template/pages/index/index.js
+++ template/pages/index/index.js
@@ -185,6 +185,10 @@ Page({
         })
     },
     tojiesuan(){    //去结算
+        wx.showLoading({
+            title: '加载中...',
+            mask: true
+        });
         let interfaceList = [];
         // console.log(this.data.interfaceList)
         this.data.interfaceList.forEach(item=>{
@@ -193,15 +197,24 @@ Page({
             interfaceDict['price'] = item.price
             interfaceDict['quantity'] = item.quantity
             interfaceDict['cindex'] = item.cindex
-            interfaceDict['image'] = item.cindex
+            // interfaceDict['image'] = item.image
             interfaceList.push(interfaceDict)
         })
         interfaceList= JSON.stringify(interfaceList)
+        // return
+        let that = this;
         wx.navigateTo({
             url:'../settlement/settlement?data='+interfaceList,  //跳转页面的路径，可带参数 ？隔开，不同参数用 & 分隔；相对路径，不需要.wxml后缀
-            success:function(){},        //成功后的回调；
-            fail:function(){},          //失败后的回调；
-            complete:function(){},      //结束后的回调(成功，失败都会执行)
+            success:function(){
+            },        //成功后的回调；
+            fail:function(){
+
+            },          //失败后的回调；
+            complete:function(){
+                console.log('asdasdasd')
+                that.hideLoading()
+
+            },      //结束后的回调(成功，失败都会执行)
         })
     },
     showModal(e) {  //显示购物车
--- template/pages/settlement/settlement.js
+++ template/pages/settlement/settlement.js
@@ -5,16 +5,20 @@ Page({
      */
     data: {
         interfaceList:[],   //上一页用户传入的商品list
+        shopSum:0,  //商品价格总和
+        receipt:1
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-       
-        // this.setData({
-        //     interfaceList:JSON.parse(options.data),
-        // });
+       let interfaceList = JSON.parse(options.data)
+        this.setData({
+            interfaceList:interfaceList,
+        });
+        console.log(this)
+        this.totalpriceFn()
     },
 
     /**
@@ -22,6 +26,38 @@ Page({
      */
     onReady: function () {
 
+    },
+    totalpriceFn(){ //计算商品总价
+        let shoppriceList = [];
+        let shopSum = 0
+        this.data.interfaceList.forEach(item => {
+            shoppriceList.push(item.price*item.quantity)
+        });
+        shopSum = shoppriceList.reduce((n,m)=>{
+            return n + m;
+        })
+        console.log('shopSum: ', shopSum);
+        this.setData({
+            shopSum : shopSum
+        })
+    },
+    addAddressFn(){
+        wx.navigateTo({
+            url:'../address/address',  //跳转页面的路径，可带参数 ？隔开，不同参数用 & 分隔；相对路径，不需要.wxml后缀
+            success:function(){},        //成功后的回调；
+            fail:function(){},          //失败后的回调；
+            complete:function(){},      //结束后的回调(成功，失败都会执行)
+        })
+    },
+    showModal(e) {
+        this.setData({
+            modalName: e.currentTarget.dataset.target
+        })
+    },
+    hideModal(e) {
+        this.setData({
+            modalName: null
+        })
     },
 
     /**
